a4ae3f6f3dc56524c8e47d1a824eaee89f947f92
Yomnasalama <64813394+Yomnasalama@users.noreply.github.com>
2021-06-05 13:17:44 +0200

    Add files via upload

---
+++ lcd_test.c
@@ -0,0 +1,29 @@
+//intialize to screen of lcd
+void lcd_init(){
+	  lcd_data_ports_init();
+    lcd_control_ports_init();
+	  lcd_command(0x30);   //wakeup 
+    Delay();
+    lcd_command(0x38);   //8- bits data
+    Delay();
+    lcd_command(0x01);    //clear
+    Delay();
+    lcd_command(0x80);   //force cursor to beginging of first line
+    Delay();
+    lcd_command(0x0F);    //display on
+    Delay();
+}
+
+void display_distance(int x)
+{
+	 char h,t,u;
+	 h = (x / 100) + '0';
+	 lcd_data(h);
+   Delay();
+	 t = ((x / 10)% 10)+ '0';
+	 lcd_data(t);
+   Delay();
+	 u = (x % 10) + '0';
+	 lcd_data(u);
+   Delay();
+}
